ff685f6b2cea99522e7dde7ebddf0b006ff9550c
frank <mainafrank400@gmail.com>
2024-06-07 22:27:06 +0300

    created the login modal

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [placeholder, setPlaceholder] = useState({
+    username: 'Username',
+    password: 'Password'
+  })
+
   return (
-    <div className="login">
-      <h1>login</h1>
-    </div>
+    <div className='login'>
+    <form action="">
+    <input type="text"  className="txt"
+        onChange={(e) => setPlaceholder(prev => (
+          {
+            ...prev,
+            username: e.target.value
+          }
+        ))} id="" placeholder={placeholder.username} />
+    
+      <br />
+      <br />
+      <input type="password" className="txt" 
+          onChange={(e) => setPlaceholder(prev => (
+            {
+              ...prev,
+              password: e.target.value
+            }
+          ))} placeholder={placeholder.password}/>
+      
+      <br />
+      <div className="forget">
+        <input type="checkbox" name="" className="Remember" /> &nbsp;<p className="rem">Remember me</p>
+
+        <a href="pass" className="pass">Forgot Password?</a>
+      </div>
+      <button type="submit" className='log'>Login</button>
+    </form>
+    
+     <p>Don't have an account? <a href="register" className='reg'>Register</a></p>
+   
+  </div>
   );
 }
 
