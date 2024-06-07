6544c1beaaee190b137f7568905d708aef76b785
frank <mainafrank400@gmail.com>
2024-06-07 23:15:31 +0300

    Added icons on inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Login() {
   const [placeholder, setPlaceholder] = useState({
-    username: 'Username',
-    password: 'Password'
-  })
+    username: "Username",
+    password: "Password",
+  });
 
   return (
-    <div className='login'>
-    <form action="">
-    <input type="text"  className="txt"
-        onChange={(e) => setPlaceholder(prev => (
-          {
-            ...prev,
-            username: e.target.value
-          }
-        ))} id="" placeholder={placeholder.username} />
-    
-      <br />
-      <br />
-      <input type="password" className="txt" 
-          onChange={(e) => setPlaceholder(prev => (
-            {
-              ...prev,
-              password: e.target.value
+    <div className="login">
+      <form action="">
+        <div className="flex">
+          <img src="\images\user.png" alt="" className="icon" />
+          <input
+            type="text"
+            className="txt"
+            onChange={(e) =>
+              setPlaceholder((prev) => ({
+                ...prev,
+                username: e.target.value,
+              }))
             }
-          ))} placeholder={placeholder.password}/>
-      
-      <br />
-      <div className="forget">
-        <input type="checkbox" name="" className="Remember" /> &nbsp;<p className="rem">Remember me</p>
+            id=""
+            placeholder={placeholder.username}
+          />
+        </div>
 
-        <a href="pass" className="pass">Forgot Password?</a>
-      </div>
-      <button type="submit" className='log'>Login</button>
-    </form>
-    
-     <p>Don't have an account? <a href="register" className='reg'>Register</a></p>
-   
-  </div>
+        <br />
+
+        <div className="flex">
+          <img src="\images\padlock.webp" alt="" className="icon" />
+          <input
+            type="password"
+            className="txt"
+            onChange={(e) =>
+              setPlaceholder((prev) => ({
+                ...prev,
+                password: e.target.value,
+              }))
+            }
+            placeholder={placeholder.password}
+          />
+        </div>
+
+        <div className="forget space">
+          <input type="checkbox" name="" className="Remember" /> &nbsp;
+          <p className="rem">Remember me</p>
+          <a href="pass" className="pass">
+            Forgot Password?
+          </a>
+        </div>
+        <button type="submit" className="log space">
+          Login
+        </button>
+      </form>
+
+      <p className="mg">
+        Don't have an account?{" "}
+        <a href="register" className="reg space">
+          Register
+        </a>
+      </p>
+    </div>
   );
 }
 
